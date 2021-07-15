44e4118f8b82b41c14c4651534e2a75d0096f517
Daniel Atanasov <daniel.a.atanasov97@gmail.com>
2021-07-15 09:18:20 +0300

    feat: Add better compiler support and update release arguments for MSVC

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -1,6 +1,20 @@
 cmake_minimum_required(VERSION 3.15)
 cmake_policy(SET CMP0091 NEW)
 
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    if (CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+        set(IS_CLANGCL true)
+    else()
+        set(IS_CLANG true)
+    endif()
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(IS_GCC true)
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+    set(IS_INTEL true)
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    set(IS_MSVC true)
+endif()
+
 macro(setup target)
     # COMPILE
     # LINK
@@ -11,29 +25,35 @@ macro(setup target)
     # RELDEB_COMPILE
     # RELDEB_LINK
 
-    if(MSVC)
+    if(DEFINED IS_CLANGCL OR DEFINED IS_MSVC)
+        string(REGEX REPLACE "/Ob1" "/Ob3" CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+        string(REGEX REPLACE "/Ob1" "/Ob3" CMAKE_C_FLAGS_RELWITHDEBINFO   "${CMAKE_C_FLAGS_RELWITHDEBINFO}")
+        string(REGEX REPLACE "/Ob2" "/Ob3" CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+        string(REGEX REPLACE "/Ob2" "/Ob3" CMAKE_C_FLAGS_RELWITHDEBINFO   "${CMAKE_C_FLAGS_RELWITHDEBINFO}")
+
         set(COMPILE /MP /cgthreads8 /Zc:preprocessor /wd5105)
         set(LINK /INCREMENTAL:NO /CGTHREADS:8)
 
         set(DEBUG_COMPILE /JMC /Od /MDd)
 
-        set(RELEASE_COMPILE /O2 /GS- /guard:cf /D NDEBUG /JMC- /GL /Ob2 /MD /fp:fast)
+        set(RELEASE_COMPILE /O2 /GS- /guard:cf /D NDEBUG /JMC- /GL /Ob3 /MD /fp:fast)
         set(RELEASE_LINK /OPT:REF /OPT:ICF=10000 /LTCG)
 
         set(RELDEB_COMPILE ${RELEASE_COMPILE})
         set(RELDEB_LINK ${RELEASE_LINK})
     else()
-        set(
-            COMPILE
+        set(COMPILE
             -Wno-deprecated-comma-subscript
             -Wno-macro-redefined
             -Wno-address-of-temporary
             -fmacro-backtrace-limit=0
             -fexceptions
+            -fdiagnostics-color
         )
-        set(DEBUG_COMPILE -O0 -g -gcodeview)
+        set(LINK -Xlinker /subsystem:console)
+        set(DEBUG_COMPILE -O0 -g)
         set(RELEASE_COMPILE -Ofast -flto -march=native -DNDEBUG)
-        set(RELDEB_COMPILE ${RELEASE_COMPILE} -g -gcodeview)
+        set(RELDEB_COMPILE ${RELEASE_COMPILE} -g)
     endif()
 
     target_compile_definitions(${target} PUBLIC _CRT_SECURE_NO_WARNINGS)
